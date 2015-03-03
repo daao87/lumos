fc1bc43ba7a5c40b272ac3fb2627cb3d6316d2f8
Diego Alvarado <daao87@gmail.com>
2015-03-03 14:55:53 +0100

    add voice recognition

--- app/src/main/java/com/h4ackademy/hp/lumos/MainActivity.java
+++ app/src/main/java/com/h4ackademy/hp/lumos/MainActivity.java
@@ -1,28 +1,58 @@
 package com.h4ackademy.hp.lumos;
 
+import android.content.Intent;
 import android.hardware.Camera;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.hardware.Camera.Parameters;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
 
     private Camera cam;
     private Parameters p;
 
+    private TextView mText;
+    private SpeechRecognizer sr;
+    private static final String TAG = "MyStt3Activity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button speakButton = (Button) findViewById(R.id.btn_speak);
+        mText = (TextView) findViewById(R.id.textView1);
+
+        sr = SpeechRecognizer.createSpeechRecognizer(this);
+        sr.setRecognitionListener(new Listener());
+
+        speakButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.getId() == R.id.btn_speak) {
+                    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                    intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,"voice.recognition.test");
+                    intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,5);
+                    sr.startListening(intent);
+                }
+            }
+        });
 
     }
 
-    public void turn_On_flash(View view){
+    public void turnOnFlash(View v){
         cam = Camera.open();
         p = cam.getParameters();
         p.setFlashMode(Parameters.FLASH_MODE_TORCH);
@@ -30,11 +60,10 @@ public class MainActivity extends ActionBarActivity {
         cam.startPreview();
     }
 
-    public void turn_Off_flash(View view){
+    public void turnOffFlash(View v){
         cam.stopPreview();
         cam.release();
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -56,5 +85,56 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    class Listener implements RecognitionListener
+    {
+        public void onReadyForSpeech(Bundle params) {
+            Log.d(TAG, "onReadyForSpeech");
+        }
+        public void onBeginningOfSpeech() {
+            Log.d(TAG, "onBeginningOfSpeech");
+        }
+        public void onRmsChanged(float rmsdB) {
+            Log.d(TAG, "onRmsChanged");
+        }
+        public void onBufferReceived(byte[] buffer) {
+            Log.d(TAG, "onBufferReceived");
+        }
+        public void onEndOfSpeech() {
+            Log.d(TAG, "onEndofSpeech");
+        }
+        public void onError(int error) {
+            Log.d(TAG,  "error " +  error);
+            mText.setText("error " + error);
+        }
+        public void onDestroy(){
+            if(sr!=null) {
+                sr.destroy();
+            }
+        }
+        public void onResults(Bundle results) {
+            String str = new String();
+            Log.d(TAG, "onResults " + results);
+            ArrayList data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            for (int i = 0; i < data.size(); i++) {
+                Log.d(TAG, "result " + data.get(i));
+                str += data.get(i);
+            }
+            String strings = str.toLowerCase();
+            if(strings.contains("lumos") || strings.contains("loomis") || strings.contains("luminous")) {
+                turnOnFlash(mText);
+            }
+            if(strings.contains("knox") || strings.contains("nox") || strings.contains("knocks")) {
+                turnOffFlash(mText);
+            }
+            mText.setText("results: " + String.valueOf(data.size()) + str);
+        }
+        public void onPartialResults(Bundle partialResults) {
+            Log.d(TAG, "onPartialResults");
+        }
+        public void onEvent(int eventType, Bundle params) {
+            Log.d(TAG, "onEvent " + eventType);
+        }
     }
 }
